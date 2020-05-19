bb12920814b3cd1e85093bc9c785dae763900818
RoxanaCortez <00502211@uca.edu.sv>
2020-05-19 12:24:24 -0600

    frase dividida en lineas

--- tarea3/tarea_3_00502211.asm
+++ tarea3/tarea_3_00502211.asm
@@ -4,7 +4,10 @@ section .text
 
 	call 	texto	
 	call 	cursor
-	call 	phrase
+	call 	primera
+	call 	segunda
+	call	tercera
+	call	cuarta
 	call	kbwait
 
 	int 	20h
@@ -35,21 +38,53 @@ kbwait: mov 	ax, 0000h
 
 m_cursr:mov 	ah, 02h
 	mov 	dx, di  ; columna
-	mov 	dh, 12d ; fila
+	mov 	dh, [202h] ; fila
 	mov 	bh, 0h
 	int 	10h
 	ret
 
-phrase:	mov 	di, 29d
-lupi:	mov 	cl, [msg+di-29d]
+primera:mov 	di, 0d
+conta1: mov 	cl, [msg1+di-0d]
 	call    m_cursr
 	call 	w_char
 	inc	di
-	cmp 	di, len
-	jb	lupi
+	cmp 	di, len1
+	jb	conta1
 	ret
 
+segunda:mov 	di, 25d
+conta2:	mov 	cl, [msg2+di-25d]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len2
+	jb	conta2
+	ret
+
+tercera:mov 	di, 50d
+conta3:	mov 	cl, [msg3+di-50d]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len3
+	jb	conta3
+	ret
+
+cuarta:	mov 	di, 55d
+conta4:	mov 	cl, [msg4+di-55d]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len4
+	jb	conta4
+	ret
 
 section .data
-msg	db 	"Mami que tu quiere? "
-len 	equ	$-msg+29d
+msg1	db 	"SI ERES HUMILDE NADA TE TOCARA"
+len1 	equ	$-msg1+0d
+msg2	db	"NI LA ALABANZA NI LA DESGRACIA"
+len2	equ	$-msg2+25d
+msg3 	db	"POR QUE SABES LO QUE ERES" 
+len3	equ	$-msg3+50d
+msg4 	db	"Madre Teresa de Calcuta" 
+len4	equ	$-msg4+55d
