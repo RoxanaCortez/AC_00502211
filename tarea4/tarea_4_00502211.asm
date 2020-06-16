bb106edd76593f86d05ea9ff97e78ff971906c2c
RoxanaCortez <00502211@uca.edu.sv>
2020-06-16 17:14:10 -0600

    iniciando ejercicio

---
+++ tarea4/tarea_4_00502211.asm
@@ -0,0 +1,31 @@
+org 	100h
+
+section .text
+
+	xor 	si, si 	;lo mimso que: mov si, 0000h
+lupi:	call 	kb
+	cmp 	al, "$"
+	je	mostrar
+	mov	[300h+si], al ; CS:0300h en adelante
+	inc 	si
+	jmp 	lupi
+
+mostrar:mov	byte [300h+si], "$"
+	call 	w_strng
+
+	call 	kb	; solo detenemos la ejecución
+	int 	20h
+
+texto:	mov 	ah, 00h
+	mov	al, 03h
+	int 	10h
+	ret
+
+kb: 	mov	ah, 7h
+	int 	21h
+	ret
+
+w_strng:mov	ah, 09h
+	mov 	dx, 300h
+	int 	21h
+	ret
